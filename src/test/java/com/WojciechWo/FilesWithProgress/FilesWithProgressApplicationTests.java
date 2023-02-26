af036efbc0f49a6a5d4dfae0875d43b40f0210db
Wołoszyn Wojciech <wojtek201990@gmail.com>
2023-02-26 14:56:00 +0100

    Initial commit

---
+++ src/test/java/com/WojciechWo/FilesWithProgress/FilesWithProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WojciechWo.FilesWithProgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesWithProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
